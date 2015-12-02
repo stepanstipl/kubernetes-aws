7a56c89974f82f6f35054d470773fa85af41d4a3
Stepan Stipl <stepan@stipl.net>
2015-12-02 15:22:30 +0000

    Initial commit

---
+++ drk-setup-master/README.md
@@ -0,0 +1,4 @@
+# drk-setup-master
+
+This will try to find and mount EBS persistent disk on kubernetes master nodes.
+Expects /dev/xvdb and mounts to /srv.
---
+++ drk-setup-master/setup-master.sh
@@ -0,0 +1,25 @@
+!/bin/bash
+
+echo "Waiting for master persistent disk to be attached"
+attempt=0
+while true; do
+  echo "Attempt $(($attempt+1)) to check for ${DRK_DEVICE}"
+  if [[ -e ${DRK_DEVICE} ]]; then
+    echo "Found ${DRK_DEVICE}"
+    break
+  fi
+  attempt=$(($attempt+1))
+  sleep 1
+done
+
+# Check if we have fs in the fstab already
+echo "Checking if persistent disk is configured"
+if (! grep -q "^${DRK_DEVICE}" /etc/fstab); then
+
+  # Mount Master Persistent Disk
+  echo "Mounting master persistend Disk"
+  mkdir -p ${DRK_MOUNTPOINT}
+  mkfs -t ext4 ${DRK_DEVICE}
+  echo "${DRK_DEVICE} ${DRK_MOUNTPOINT} ext4 noatime  0 0" >> /etc/fstab
+  mount ${DRK_MOUNTPOINT}
+fi
