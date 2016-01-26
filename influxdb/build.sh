31a28e2225fef19f4126c5b3dfd63218ea03249d
Stepan Stipl <stepan@stipl.net>
2016-01-26 18:58:53 +0000

    One more

--- influxdb/build.sh
+++ influxdb/build.sh
@@ -17,11 +17,12 @@ cp -r ./usr/bin/influxd ./usr/bin/influx "${OUTPUT_DIR}/"
 # Needed for collectd
 wget -O "${OUTPUT_DIR}/types.db" "https://raw.githubusercontent.com/collectd/collectd/master/src/types.db"
 
+
 # Get kubernetes
-wget "https://github.com/kubernetes/kubernetes/releases/download/v${K8S_VERSION}/kubernetes.tar.gz"
-tar -xvzf kubernetes.tar.gz
+wget "https://github.com/kubernetes/kubernetes/archive/v${K8S_VERSION}.tar.gz"
+tar -xvzf "v${K8S_VERSION}.tar.gz"
 mkdir -p "${GOPATH}/src/k8s.io"
-mv "kubernetes" "${GOPATH}/src/k8s.io/"
+mv "kubernetes-1.1.4" "${GOPATH}/src/k8s.io/"
 
 cd "${GOPATH}/src/k8s.io/kubernetes"
 cp /source/influxdb-discovery.go ./
