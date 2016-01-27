64c2f87530eecbbb2e05adac3a84f9ed7fc4c6d8
Stepan Stipl <stepan@stipl.net>
2016-01-27 22:20:47 +0000

    More

--- influxdb/influxdb.sh
+++ influxdb/influxdb.sh
@@ -7,6 +7,8 @@ CURL="/usr/bin/curl ${CURL_OPTS}"
 PEER_PROTO='http'
 
 JOIN=""
+CHECK_PEERS=${CHECK_PEERS:-'true'}
+MAX_WAIT=60
 
 POD_IP=${POD_IP:?'$K8S_IP is not set'}
 
@@ -15,15 +17,22 @@ PEERS=$(/influxdb-discovery)
 
 HEALTHY_PEERS=""
 
+wait_for=$RANDOM
+let "wait_for %= $MAX_WAIT"
+sleep $wait_for
+
 for peer in $PEERS; do
-  resp_code=$($CURL "${PEER_PROTO}://${peer}/ping")
-  if [[ $resp_code == "200" ]]; then
+  resp_code='200' 
+  # If we're supposed to check peers, check them
+  [[ $CHECK_PEERS == 'true' ]] && resp_code=$($CURL "${PEER_PROTO}://${peer}/ping")
+
+  if [[ $resp_code == '200' ]]; then
     [[ -n "${HEALTHY_PEERS}" ]] && HEALTHY_PEERS="${HEALTHY_PEERS},"
     HEALTHY_PEERS="${HEALTHY_PEERS}${peer}"
   fi
 done
 
-echo "${APP}: Fiund healthy peers: ${HEALTHY_PEERS}"
+echo "${APP}: Found healthy peers: ${HEALTHY_PEERS}"
 [[ -n "${HEALTHY_PEERS}" ]] && JOIN="-join ${HEALTHY_PEERS}"
 
 export INFLUXDB_META_BIND_ADDRESS="${POD_IP}:8088"
