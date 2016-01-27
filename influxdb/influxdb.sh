46eaf493f42506d29163d96b59925bf79770a55e
Stepan Stipl <stepan@stipl.net>
2016-01-27 17:19:01 +0000

    More

--- influxdb/influxdb.sh
+++ influxdb/influxdb.sh
@@ -1,7 +1,14 @@
-#!/bin/sh +x
+#!/bin/sh -x
 APP='influxdb'
 
 JOIN=$(/influxdb-discovery)
 [[ -n "${JOIN}" ]] && JOIN="-join ${JOIN}"
+
+K8S_IP=${K8S_IP:?'$K8S_IP is not set'}
+
+export META_BIND_ADDRESS="${POD_IP}:8088"
+export META_HTTP_BIND_ADDRESS="${POD_IP}:8091"
+export ADMIN_BIND_ADDRESS="${POD_IP}:9083"
+export HTTP_BIND_ADDRESS="${POD_IP}:9086"
 
 /influxd --config /etc/influxdb.toml $JOIN
